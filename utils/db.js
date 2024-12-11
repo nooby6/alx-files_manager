a6df0c171d2dc58bf0528d4274f2324e47bedfe3
nooby6 <giftaudrine1@gmail.com>
2024-12-11 11:51:53 +0300

    Add detailed documentation and methods for user and file management in DBClient class

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,51 @@
 import { MongoClient } from 'mongodb';
 
+/**
+ * @class DBClient
+ * @classdesc A client for interacting with a MongoDB database.
+ * 
+ * @constructor
+ * Initializes a new instance of the DBClient class.
+ * 
+ * @property {boolean} isConnected - Indicates whether the client is connected to the database.
+ * @property {MongoClient} client - The MongoDB client instance.
+ * @property {Db} db - The MongoDB database instance.
+ * @property {Collection} users - The users collection.
+ * @property {Collection} files - The files collection.
+ * 
+ * @method isAlive
+ * @description Checks if the client is connected to the database.
+ * @returns {boolean} True if the client is connected, otherwise false.
+ * 
+ * @method nbUsers
+ * @description Gets the number of users in the database.
+ * @returns {Promise<number>} The number of users.
+ * 
+ * @method nbFiles
+ * @description Gets the number of files in the database.
+ * @returns {Promise<number>} The number of files.
+ * 
+ * @method getUser
+ * @description Retrieves a user from the database based on the provided arguments.
+ * @param {Object} args - The arguments to find the user.
+ * @returns {Promise<Object|null>} The user document, or null if not found.
+ * 
+ * @method addUser
+ * @description Adds a new user to the database.
+ * @param {Object} user - The user document to add.
+ * @returns {Promise<ObjectId>} The ID of the inserted user.
+ * @throws {Error} If the user already exists.
+ * 
+ * @method getFile
+ * @description Retrieves a file from the database based on the provided arguments.
+ * @param {Object} args - The arguments to find the file.
+ * @returns {Promise<Object|null>} The file document, or null if not found.
+ * 
+ * @method addFile
+ * @description Adds a new file to the database.
+ * @param {Object} file - The file document to add.
+ * @returns {Promise<ObjectId>} The ID of the inserted file.
+ */
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
